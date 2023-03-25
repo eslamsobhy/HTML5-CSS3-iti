0a5b47d234b6dfc685805aea522c76a81268ab21
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-25 15:23:02 +0200

    changed the max attribute according to the audio duration

--- day-1/Media Player/media.js
+++ day-1/Media Player/media.js
@@ -1,20 +1,37 @@
 var aud = document.querySelector("audio");
+
+window.addEventListener("load", function () {
+  var seekInput = document.getElementsByTagName("input")[0];
+  seekInput.setAttribute("max", aud.duration);
+});
+
+// play
 function playAudio() {
   aud.play();
 }
+
+// pause
 function pauseAudio() {
   aud.pause();
 }
+
+// stop
 function stopAudio() {
   aud.load();
   aud.pause();
 }
+
+// mute
 function muteAudio() {
   aud.muted = !aud.muted;
 }
+
+// change volume
 function changeVol(v) {
   aud.volume = v.value;
 }
+
+// seek
 function seek(t) {
   aud.currentTime = t.value;
 }
