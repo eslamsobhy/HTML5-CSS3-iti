666be62fa2e297858e867ea61e03a828fc911d2e
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-25 15:53:17 +0200

    changed the music by choosing from the list

--- day-1/Media Player/media.js
+++ day-1/Media Player/media.js
@@ -4,6 +4,23 @@ window.addEventListener("load", function () {
   var seekInput = document.getElementsByTagName("input")[0];
   seekInput.setAttribute("max", aud.duration);
 });
+var item = document.getElementsByClassName("item")[0];
+var item1 = document.getElementsByClassName("item")[1];
+var playerImg = document.querySelector("img");
+
+function playAmelie() {
+  item.classList.add("active");
+  item1.classList.remove("active");
+  aud.setAttribute("src", "./audios/[HQ]-La-Valse-d'Amélie-Yann-Tiersen.mp3");
+  playerImg.setAttribute("src", "./images/amelie.jpeg");
+}
+
+function playFaded() {
+  item1.classList.add("active");
+  item.classList.remove("active");
+  aud.setAttribute("src", "./audios/Alan-Walker-Faded.mp3");
+  playerImg.setAttribute("src", "./images/faded.jpeg");
+}
 
 // play
 function playAudio() {
