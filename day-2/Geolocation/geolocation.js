235c874ace03cc20b840aea4a8027d329a9941d3
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-26 13:30:38 +0200

    finished geolocation task

--- day-2/Geolocation/geolocation.js
+++ day-2/Geolocation/geolocation.js
@@ -1,7 +1,16 @@
-function initMap() {
-  const myLatLng = { lat: -25.363, lng: 131.044 };
+navigator.geolocation.getCurrentPosition(function (pos) {
+  // console.log(pos.coords.latitude);
+  // console.log(pos.coords.longitude);
+  var lati = pos.coords.latitude;
+  var longi = pos.coords.longitude;
+
+  initMap(lati, longi);
+});
+
+function initMap(x, y) {
+  const myLatLng = { lat: x, lng: y };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 8,
     center: myLatLng,
   });
 
